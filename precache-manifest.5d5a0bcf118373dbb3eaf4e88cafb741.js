d9fab3d926a72fc8a63e6543fcb87ce632933f63
Shaan Alam <shaanalam369@gmail.com>
2020-08-25 18:03:15 +0530

    Updates

---
+++ precache-manifest.5d5a0bcf118373dbb3eaf4e88cafb741.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2c82eccc3abd7f2b31cf6f2c75559e",
+    "url": "/moviehub_project/index.html"
+  },
+  {
+    "revision": "fa69e78004a2c96cce59",
+    "url": "/moviehub_project/static/css/2.1d9ddd8f.chunk.css"
+  },
+  {
+    "revision": "a3aa4c32877c46ade60c",
+    "url": "/moviehub_project/static/css/main.75d207e6.chunk.css"
+  },
+  {
+    "revision": "fa69e78004a2c96cce59",
+    "url": "/moviehub_project/static/js/2.e08ef965.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/moviehub_project/static/js/2.e08ef965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3aa4c32877c46ade60c",
+    "url": "/moviehub_project/static/js/main.fda79031.chunk.js"
+  },
+  {
+    "revision": "143bac900c29fc6058dd",
+    "url": "/moviehub_project/static/js/runtime-main.728725ed.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/moviehub_project/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/moviehub_project/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/moviehub_project/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/moviehub_project/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/moviehub_project/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/moviehub_project/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/moviehub_project/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/moviehub_project/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/moviehub_project/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/moviehub_project/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/moviehub_project/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/moviehub_project/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/moviehub_project/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/moviehub_project/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/moviehub_project/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/moviehub_project/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
